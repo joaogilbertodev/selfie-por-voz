9ba3d488e61ba61415ae9233b016313340b49bc8
joaogilbertodev <143625822+joaogilbertodev@users.noreply.github.com>
2024-07-11 17:41:31 -0300

    aula 100

--- main.js
+++ main.js
@@ -1,27 +1,60 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
 var recognition1 = new SpeechRecognition();
-function start(){
-    document.getElementById("texBox").innerHTML=""
+function start() {
+    document.getElementById("texBox").innerHTML = ""
     recognition1.start()
 }
 
-recognition1.onresult=function(event){
-    var Content=event.results[0][0].transcript
-    document.getElementById("texBox").innerHTML=Content
-    speak()
-}
+recognition1.onresult = function (event) {
+    var Content = event.results[0][0].transcript
+    document.getElementById("texBox").innerHTML = Content
+    if (Content == "tire minha selfie") {
+        speak()
+    }
 
-function speak(){
-    var synth=window.speechSynthesis
-    speakData=document.getElementById("texBox").value
-    var utterthis=new SpeechSynthesisUtterance(speakData)
+}
+var cont=0
+function speak() {
+    var synth = window.speechSynthesis
+    //speakData=document.getElementById("texBox").value
+    speakData = "tirando sua selfie em 5 segundos"
+    var utterthis = new SpeechSynthesisUtterance(speakData)
     synth.speak(utterthis)
     Webcam.attach(camera);
+    takeSnapShot()
+    setTimeout(() => {
+        takeSnapShot()
+        //save()
+    }, 9000);
+    temp(cont)
 }
-camera=document.getElementById("camera")
+
+//função para salvar
+function save(){
+    link=document.getElementById("link")
+    img=document.getElementById("selfieImg")
+    link.href=img
+    link.click()
+}
+camera = document.getElementById("camera")
 Webcam.set({
-    width: 320,
-    height: 240,
+    width: 380,
+    height: 260,
     image_format: 'jpeg',
     jpeg_quality: 90
 });
+function takeSnapShot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("foto").innerHTML = '<img id="selfieImg" src="' + data_uri + '"/>'
+    })
+}
+
+function temp(cont) {
+    for (var i = 0; i < 5; i++) {
+        cont = cont + 1
+        var synth = window.speechSynthesis;
+        speak_data = cont;
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }
+}
